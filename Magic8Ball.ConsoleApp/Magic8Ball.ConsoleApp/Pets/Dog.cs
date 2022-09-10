05e09c6e809604ad127fa0834182e99e92855e66
Jonas2312 <Jonas2312@users.noreply.github.com>
2022-09-10 14:30:38 +0200

    push

--- Magic8Ball.ConsoleApp/Magic8Ball.ConsoleApp/Pets/Dog.cs
+++ Magic8Ball.ConsoleApp/Magic8Ball.ConsoleApp/Pets/Dog.cs
@@ -8,9 +8,15 @@ namespace Magic8Ball.ConsoleApp.Pets
 {
     internal class Dog : Pet
     {
+        int BarkedToday { get; set; }
         public Dog(string name) : base(name)
         {
+            BarkedToday = 6;
+        }
 
+        public override string GetInformation()
+        {
+            return $"{base.GetInformation()} I barked {BarkedToday} times today!!";
         }
     }
 }
