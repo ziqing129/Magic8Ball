0a37dbc8b781fa3ece02f775ebdbd2bcb6cfbc44
ziqing <ziqing129@users.noreply.github.com>
2022-09-10 13:56:11 +0200

    Pet added

--- Magic8Ball.ConsoleApp/Magic8Ball.ConsoleApp/Program.cs
+++ Magic8Ball.ConsoleApp/Magic8Ball.ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Magic8Ball.ConsoleApp;
+
+var dogPet = new Pet("Milo");
+var catPet = new Pet("Songxiaogou");
+var robotPet = new Pet("Cherry");
+
+Console.WriteLine(dogPet.Name);
+Console.WriteLine(catPet.Name);
+Console.WriteLine(robotPet.Name);
